4b089928c687ad1acefb2de888d2c0c0e55e4853
Population96 <population96@gmail.com>
2016-05-23 23:34:31 -0400

    Create SQLAlchemy code for database creation in database_setup.py

---
+++ vagrant/catalog/database_setup.py
@@ -0,0 +1,64 @@
+import os
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+
+##### CODE BODY #####
+
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key = True)
+    name = Column(String(250), nullable = False)
+    email = Column(String(250), nullable = False)
+    picture = Column(String(250))
+
+
+class Category(Base):
+    __tablename__ = 'category'
+    id = Column(Integer, primary_key = True)
+    name = Column(String(80), nullable = False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+    @property
+    def serialize(self):
+        #Returns object data in easily serializeable format
+        return {
+            'id' : self.id,
+            'name' : self.name,
+            'user_id' : self.user_id,
+        }
+
+
+class Item(Base):
+    __tablename__ = 'menu_item'
+    name = Column(String(80), nullable = False)
+    id = Column(Integer, primary_key = True)
+    description = Column(String(250))
+    price = Column(String(8))
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+    category_id = Column(Integer, ForeignKey('category.id'))
+    category = relationship(Category)
+
+    @property
+    def serialize(self):
+        #Returns object data in easily serializeable format
+        return {
+            'id' : self.id,
+            'name' : self.name,
+            'description' : self.description,
+            'price' : self.price,
+            'user_id' : self.user_id,
+        }
+
+
+engine = create_engine('sqlite:///itemcatalog.db')
+Base.metadata.create_all(engine)
+
+##### END OF FILE #####
