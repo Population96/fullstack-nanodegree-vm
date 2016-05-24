31f0badc49b46394be6aa69571bf6e31b4c3f339
Population96 <population96@gmail.com>
2016-05-24 00:33:25 -0400

    Create files in appropriate directories. Add baseline code.

--- vagrant/catalog/database_setup.py
+++ vagrant/catalog/database_setup.py
@@ -37,8 +37,8 @@ class Category(Base):
 
 class Item(Base):
     __tablename__ = 'menu_item'
-    name = Column(String(80), nullable = False)
     id = Column(Integer, primary_key = True)
+    name = Column(String(80), nullable = False)
     description = Column(String(250))
     price = Column(String(8))
     user_id = Column(Integer, ForeignKey('user.id'))
@@ -55,6 +55,7 @@ class Item(Base):
             'description' : self.description,
             'price' : self.price,
             'user_id' : self.user_id,
+            'category_id' : self.category_id,
         }
 
 
